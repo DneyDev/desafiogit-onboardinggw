586d6a5be19f10a38a07f78639e35cdc850945ab
DneyDev <contdneydev@gmail.com>
2026-09-14 13:44:03 -0300

    fix: frase de html diverge do js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-//para quem for revisar: atualizações em breve
+//a última alteração feita foi a mudança no limite de valores, de 10 para 20
 
 let listaDeNumerosSorteados = [];
 let numeroLimite = 20;
@@ -13,7 +13,7 @@ function exibirTextoNaTela(tag, texto) {
 
 function exibirMensagemInicial() {
     exibirTextoNaTela('h1', 'Jogo do número secreto');
-    exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e 20');
 }
 
 exibirMensagemInicial();
